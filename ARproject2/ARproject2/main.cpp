82191aee24acd77abd53cfc3e1019abf66cee1e1
Andrej Trožić <andrejtrozic95@gmail.com>
2024-04-23 12:02:17 +0200

    update
    main function update

--- ARproject2/ARproject2/main.cpp
+++ ARproject2/ARproject2/main.cpp
@@ -36,45 +36,79 @@ int main(int argc, char* argv[]) {
     int n_for_kernel_choice;
     ConvolutionTester tester;
     bool loop = true;
+    bool testing;
 
-    while (loop) {
+    std::cout << "Zelite li da izvrsite testiranje: " << std::endl
+        << "1 za Da" << std::endl
+        << "0 za Ne" << std::endl;
+    std::cin >> testing;
 
-        std::cout << "\n\n-Odaberite Sebi Odgovarajuci Kernel-" << std::endl
-            << "1 za Identity Kernel " << std::endl
-            << "2 za Gaussian Blur Kernel" << std::endl
-            << "3 za Edge Detection Kernel" << std::endl
-            << "4 za Box Blur Kernel" << std::endl
-            << "5 za Sharpen Kernel " << std::endl
-            << "0.za izlaz" << std::endl;
-        do {
-            std::cin >> n_for_kernel_choice;
-        } while (n_for_kernel_choice < 0 || n_for_kernel_choice > 5);
+    if (testing) {
 
-        switch (n_for_kernel_choice) {
+        while (loop) {
 
-        case 1:
-            tester.runTests(inputPaths, outputPaths, kernelIdentity);
-            break;
+            std::cout << "\n\n-Odaberite Sebi Odgovarajuci Kernel-" << std::endl
+                << "1 za Identity Kernel " << std::endl
+                << "2 za Gaussian Blur Kernel" << std::endl
+                << "3 za Edge Detection Kernel" << std::endl
+                << "4 za Box Blur Kernel" << std::endl
+                << "5 za Sharpen Kernel " << std::endl
+                << "0.za izlaz" << std::endl;
+            do {
+                std::cin >> n_for_kernel_choice;
+            } while (n_for_kernel_choice < 0 || n_for_kernel_choice > 5);
 
-        case 2:
-            tester.runTests(inputPaths, outputPaths, kernelGaussianBlur);
-            break;
+            switch (n_for_kernel_choice) {
 
-        case 3:
-            tester.runTests(inputPaths, outputPaths, kernelEdgeDetection);
-            break;
+            case 1:
+                std::cout << "Konvolucija slike koristenjem korisnicki definisane funkcije: " << std::endl;
+                tester.runTests1(inputPaths, outputPaths, kernelIdentity);
+                std::cout << "Konvolucija slike koristenjem funkcije iz opecv biblioteke: " << std::endl;
+                tester.runTests2(inputPaths, outputPaths, kernelIdentity);
+                std::cout << "Konvolucija slike koristenjem funkcije iz opecv(SIMD optimized) biblioteke: " << std::endl;
+                tester.runTests3(inputPaths, outputPaths, kernelIdentity);
+                break;
 
-        case 4:
-            tester.runTests(inputPaths, outputPaths, kernelBoxBlur);
-            break;
+            case 2:
+                std::cout << "Konvolucija slike koristenjem korisnicki definisane funkcije: " << std::endl;
+                tester.runTests1(inputPaths, outputPaths, kernelGaussianBlur);
+                std::cout << "Konvolucija slike koristenjem funkcije iz opecv biblioteke: " << std::endl;
+                tester.runTests2(inputPaths, outputPaths, kernelGaussianBlur);
+                std::cout << "Konvolucija slike koristenjem funkcije iz opecv(SIMD optimized) biblioteke: " << std::endl;
+                tester.runTests3(inputPaths, outputPaths, kernelGaussianBlur);
+                break;
 
-        case 5:
-            tester.runTests(inputPaths, outputPaths, kernelSharpen);
-            break;
+            case 3:
+                std::cout << "Konvolucija slike koristenjem korisnicki definisane funkcije: " << std::endl;
+                tester.runTests1(inputPaths, outputPaths, kernelEdgeDetection);
+                std::cout << "Konvolucija slike koristenjem funkcije iz opecv biblioteke: " << std::endl;
+                tester.runTests2(inputPaths, outputPaths, kernelEdgeDetection);
+                std::cout << "Konvolucija slike koristenjem funkcije iz opecv(SIMD optimized) biblioteke: " << std::endl;
+                tester.runTests3(inputPaths, outputPaths, kernelEdgeDetection);
+                break;
 
-        case 0:
-            loop = false;
-            break;
+            case 4:
+                std::cout << "Konvolucija slike koristenjem korisnicki definisane funkcije: " << std::endl;
+                tester.runTests1(inputPaths, outputPaths, kernelBoxBlur);
+                std::cout << "Konvolucija slike koristenjem funkcije iz opecv biblioteke: " << std::endl;
+                tester.runTests2(inputPaths, outputPaths, kernelBoxBlur);
+                std::cout << "Konvolucija slike koristenjem funkcije iz opecv(SIMD optimized) biblioteke: " << std::endl;
+                tester.runTests3(inputPaths, outputPaths, kernelBoxBlur);
+                break;
+
+            case 5:
+                std::cout << "Konvolucija slike koristenjem korisnicki definisane funkcije: " << std::endl;
+                tester.runTests1(inputPaths, outputPaths, kernelSharpen);
+                std::cout << "Konvolucija slike koristenjem funkcije iz opecv biblioteke: " << std::endl;
+                tester.runTests2(inputPaths, outputPaths, kernelSharpen);
+                std::cout << "Konvolucija slike koristenjem funkcije iz opecv(SIMD optimized) biblioteke: " << std::endl;
+                tester.runTests3(inputPaths, outputPaths, kernelSharpen);
+                break;
+
+            case 0:
+                loop = false;
+                break;
+            }
         }
     }
 
